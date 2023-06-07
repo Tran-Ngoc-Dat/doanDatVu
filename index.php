99114f0b1d90edb3466a59b57ae05579eebb5bc9
TranNgocDat <62785050+Tran-Ngoc-Dat@users.noreply.github.com>
2023-06-07 19:04:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+define('LIBRARIES', './libraries/');
+define('SOURCES', './sources/');
+define('LAYOUT', 'layout/');
+define('THUMBS', 'thumbs');
+define('TEMPLATE', './templates/');
+
+require_once LIBRARIES . "config.php";
+require_once LIBRARIES . 'autoload.php';
+new AutoLoad();
+$d = new PDODb($config['database']);
+$flash = new Flash();
+$router = new AltoRouter();
+$func = new Functions($d, $cache);
+$cart = new Cart($d);
+/* Router */
+require_once LIBRARIES . "router.php";
+include TEMPLATE . "index.php";
+
+?>
